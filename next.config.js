1115f851037590a60dff2c19c38fcbb22384243d
Raiyan P. Haron <haron.rp@ad.sprobe.ph>
2021-10-24 21:57:14 +0800

    change domain name for vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
     DATABASE_URI: "mongodb+srv://raiyanpharon:_Password123@cluster0.pdw8e.mongodb.net/myStoreAppV2?retryWrites=true&w=majority",
-    DOMAIN_NAME: "http://localhost:3000"
+    DOMAIN_NAME: "https://my-store-app-v2.vercel.app"
   },
   reactStrictMode: true,
   eslint: {
