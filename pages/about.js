a8424b916d6b1b139eb05d03faefe2284f3e6e94
Raiyan P. Haron <haron.rp@ad.sprobe.ph>
2021-10-24 22:32:52 +0800

    Implement about page

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,18 @@
-
+import Link from 'next/link'
 
 const about = () => {
     return (
         <>
-            About
+            <h2>About me 👩🏻‍💻</h2>
+            <p>This is a demo app. A simple online demo store.</p>
+            <Link href='/new'>
+                <a><span>Click here to create your store.</span></a>
+            </Link>
+            <h3>Research &amp; Development project using NextJS</h3>
+            <h4>with REST API, MongoD</h4>
+            by
+            <p>Raiyan Haron | hrdevs@gmail.com</p>
+            ©️ 2021
         </>
     )
 }
